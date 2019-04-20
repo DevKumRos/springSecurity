671898aa520e52a6c1b2f2f975e255bff6155839
Mr.Kumar <kumar22.bms@gmail.com>
2019-04-20 23:32:46 +0530

    Spring Security Implment

---
+++ src/main/resources/sql/Data.sql
@@ -0,0 +1,5 @@
+INSERT INTO user(id,username,password, role)VALUES(1,'Kumar ','12345', 'USER');
+INSERT INTO user(id,username,password, role)VALUES(2,'Devaraj','12345', 'USER');
+INSERT INTO user(id,username,password, role)VALUES(3,'Monika','12345', 'USER');
+INSERT INTO user(id,username,password, role)VALUES(4,'Naveen','12345', 'USER');
+INSERT INTO user(id,username,password, role)VALUES(6,'Anil','12345', 'USER');
